5fc78a5fc88006421107eee6addc21f7afb59416
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2024-09-26 20:56:49 +0200

    Initial Commit

---
+++ InternaliOSApisDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InternaliOSApisDemo
+//
+//  Created by Malte Ruff on 26.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InternaliOSApisDemo/InternaliOSApisDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternaliOSApisDemoApp.swift
+//  InternaliOSApisDemo
+//
+//  Created by Malte Ruff on 26.09.24.
+//
+
+import SwiftUI
+
+@main
+struct InternaliOSApisDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
